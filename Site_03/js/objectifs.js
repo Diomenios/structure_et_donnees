4a06ca1581bcdae937e4407a3f167022ed915cb1
diomenios <loui.arys@gmail.com>
2019-04-01 17:06:59 +0200

    fin devoir tp08

--- Site_03/js/objectifs.js
+++ Site_03/js/objectifs.js
@@ -19,23 +19,34 @@ function soumettreRequete(event) {                         // recoit l'évèneme
 /* ***** appel ajax ***** */
 function envoyerRequete(listOfValue) {
     let xhr = new XMLHttpRequest();                                                             // création d'un objet servant à effectuer la requête Ajax
-    xhr.open('get', constructUrl(["ville", "mois", "objectifMin"], "http://10.4.35.183/objectifsVendeurs?" ,listOfValue), true);    // définit le type (get), l'url et l'asynchrone de la requête
+    xhr.open('get', constructUrl(["ville", "mois", "objectifMin"], "http://localhost:80/objectifsVendeurs?" ,listOfValue), true);    // définit le type (get), l'url et l'asynchrone de la requête
       xhr.onload =                                                                                // définit la fonction à exécuter lorsque la réponse arrivera (le callback)
         function traiterReponse() {
-            document.querySelector("#footer_table").innerHTML = xhr.responseText;                 // ici, place simplement la réponse dans un élément de la page
+            document.querySelector("#footer_table").innerHTML = formatTable(JSON.parse(xhr.responseText));                 // ici, place simplement la réponse dans un élément de la page
         };
     xhr.send();                                                                                 // envoit la requête, lorsqu'elle est bien prête et définie
 }
 
-//list et paramValue value doivent avoir la même valeur 
+//list et paramValue value doivent avoir la même valeur
 function constructUrl(list, url, paramValue){
 	let returnString = url;
-	
+
 	for(let e = 0 ; e < list.length-1 ; e++){
 		returnString += list[e] + "=" + paramValue[e] + "&";
 	}
-	returnString += list[e] + "=" + paramValue[e];
-	
+	returnString += list[list.length-1] + "=" + paramValue[list.length-1];
+
 	return returnString;
 }
 
+function formatTable(list){
+  let table = "<table> <tr> <th> id </th> <th> nom </th> <th> prenom </th>" +
+   "<th> mois </th> <th> objectif </th> </tr>";
+
+   for(let e of list){
+     table += "<tr> <td>" + e.id + "</td> <td>"+ e.nom + "</td> <td>"+ e.prenom +
+              "</td> <td>"+ e.mois + "</td> <td>"+ e.objectif + "</td> </tr>";
+   }
+
+   return table;
+}
